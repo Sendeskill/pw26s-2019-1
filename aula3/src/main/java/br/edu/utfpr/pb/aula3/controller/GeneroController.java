65dc439d6baa231181880095902bcd0e2231e549
Vinicius Pegorini <viniciuspegorini@gmail.com>
2019-03-25 23:00:55 -0300

    commit aula3 2019-03-25 23h00

---
+++ aula3/src/main/java/br/edu/utfpr/pb/aula3/controller/GeneroController.java
@@ -0,0 +1,27 @@
+package br.edu.utfpr.pb.aula3.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.utfpr.pb.aula3.model.Genero;
+import br.edu.utfpr.pb.aula3.service.GeneroService;
+
+@RestController
+@RequestMapping("genero")
+public class GeneroController {
+	
+	@Autowired
+	private GeneroService generoService;
+	
+	@GetMapping
+	@ResponseBody
+	public List<Genero> findAll(){
+		return generoService.findAll();
+	}
+	
+}
---
+++ aula3/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+
+insert into genero (nome) values ('Drama');
+insert into genero (nome) values ('Suspense');
+insert into genero (nome) values ('Terror');
