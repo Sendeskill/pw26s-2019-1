b02f5f43d6b133d5712e79c0a1d26123feac5b1f
Vinicius Pegorini <viniciuspegorini@gmail.com>
2019-03-27 00:00:37 -0300

    commit aula3 2019-03-26 23h00

--- aula3/src/main/java/br/edu/utfpr/pb/aula3/controller/GeneroController.java
+++ aula3/src/main/java/br/edu/utfpr/pb/aula3/controller/GeneroController.java
@@ -2,9 +2,15 @@ package br.edu.utfpr.pb.aula3.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,4 +36,23 @@ public class GeneroController {
 		return generoService.findOne(id);
 	}
 	
+	@PostMapping
+	public Genero save(@RequestBody @Valid Genero genero) {
+		return generoService.save(genero);
+	}
+
+	@PutMapping
+	public Genero update(@RequestBody @Valid Genero genero) {
+		return generoService.save(genero);
+	}
+	
+	@DeleteMapping("{id}")
+	public void delete(@PathVariable Integer id) {
+		generoService.delete(id);
+	} 
+	
+	@GetMapping("count")
+	public long count() {
+		return generoService.count();
+	}
 }
