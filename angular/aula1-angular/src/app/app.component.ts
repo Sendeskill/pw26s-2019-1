0aa874f9c13a6e261ba3b8e3075724d34bb980c8
Vinicius Pegorini <viniciuspegorini@gmail.com>
2019-04-02 23:41:34 -0300

    commit aula1-angular 2019-04-02 22h10

---
+++ angular/aula1-angular/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  titulo = 'Aula 1 - Angular';
+  titulo2 = 'Top. Avançados em Prog. Web';
+}
