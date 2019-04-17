d3a66df8ab6c799a9754a6c811620198be2bd91b
Vinicius Pegorini <viniciuspegorini@gmail.com>
2019-04-16 23:37:00 -0300

    commit aula3-angular 2019-04-16 23h00

---
+++ angular/aula3-angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { PrincipalComponent } from './principal/principal.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    PrincipalComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
